97001f6f684540b580e5c4945c32ca209f2f6858
mpinia76 <marcos.pinero1976@gmail.com>
2023-11-28 15:14:31 -0300

    varios

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+// app/Http/Controllers/DashboardController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+
+            return view('dashboard');
+
+    }
+}
+
